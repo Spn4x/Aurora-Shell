2463ff4a84950741af0e5123d90782d3da622a79
Spn4x <johneric.je10@gmail.com>
2026-04-26 12:11:18 +0800

    Removed the old launcher

--- widgets/launcher/launcher.h
+++
@@ -1,9 +0,0 @@
-#ifndef LAUNCHER_H
-#define LAUNCHER_H
-
-#include <gtk/gtk.h>
-
-// The public entry point for the orchestrator
-G_MODULE_EXPORT GtkWidget* create_widget(const char *config_string);
-
-#endif // LAUNCHER_H
